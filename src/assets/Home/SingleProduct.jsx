8f7abe8a13a231301f4d6d9c21643f4aafc87888
AsadUllahShaikh123 <asadullahshaikh713@gmail.com>
2022-02-22 19:26:09 +0500

    add to remove from cart design done

--- src/assets/Home/SingleProduct.jsx
+++ src/assets/Home/SingleProduct.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import { CartState } from "../context/Context";
+import Rating from "./Rating";
 
-const SingleProduct = ({product}) => {
+const SingleProduct = ({ product }) => {
+  let {
+    state: { cart },
+    dispatch,
+  } = CartState();
+
   return (
-    <h3>{product.name}</h3>
-  )
-}
+    <div className="products">
+      <Card>
+        <Card.Img variant="top" src={product.image} />
+        <Card.Body>
+          <Card.Title>{product.name}</Card.Title>
+          <Card.Subtitle style={{ paddingBottom: "10px" }}>
+            <span>Rs {product.price.split(".")[0]}</span>
+            {product.fastDelivery ? (
+              <div>Fast Delivery</div>
+            ) : (
+              <div>4 Days Delivery</div>
+            )}
+            <Rating rating={product.ratings} />
+          </Card.Subtitle>
+          {cart.some((p) => p.id === product.id) ? (
+            <Button
+              onClick={() =>
+                dispatch({
+                  type: "REMOVE_FROM_CART",
+                  payload: product,
+                })
+              }
+              variant="danger"
+            >
+              Remove From Cart
+            </Button>
+          ) : (
+            <Button onClick={()=> 
+              dispatch({
+                type:"ADD_TO_CART",
+                payload:product
+              })
+            } disabled={!product.inStock}>
+              {!product.inStock ? "Out Of Stock" : "Add to Cart"}
+            </Button>
+          )}
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
 
-export default SingleProduct
+export default SingleProduct;
